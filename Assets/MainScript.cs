fc9fac8d2100250c6218e030f177de665d3090ec
Natane <natane-b@hotmail.fr>
2019-10-30 16:18:08 +0100

    First comit

---
+++ Assets/MainScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainScript : MonoBehaviour
+{
+    [SerializeField]
+    private Transform[] cubes;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       Debug.Log("hello word"); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
